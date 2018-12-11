ac3a76308bb45cb9072b04a40e39a11a32935bf4
Administrator <Administrator@DESKTOP-3DAE0Q4>
2018-12-11 18:50:46 +0530

    First project commit

---
+++ Test Suites/User_Test_Suite_1.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>User_Test_Suite_1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-12-11T17:54:41</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>ec0f9cbb-b700-4da0-b0a9-ea8ac0a2ec9a</testSuiteGuid>
+   <testCaseLink>
+      <guid>9b414ad1-4c11-47b3-8d16-d620030fc449</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ListNewUsers</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d9a7035a-2179-4fb0-8f8e-bdeba5cf6d2a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestHTTPMethods</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
